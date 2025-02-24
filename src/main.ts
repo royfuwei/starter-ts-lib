bb7e053822c7db5f636baf3a17a9a8be3b64659f
royfuwei <royfuwei@gmail.com>
2025-02-24 15:16:26 +0800

    chore: add dotenv-flow

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import 'module-alias/register';
 import DemoUtils from '@/utils/demo.utils';
+import { configs } from './configs';
 
 const main = async () => {
   const demoValue = DemoUtils.getDemoValue();
+  console.log(`APP_NAME: ${configs.name}`);
+  console.log(`NODE_ENV: ${configs.env}`);
   console.log(`${demoValue}!!`);
   await new Promise((resolve) => setTimeout(resolve, 5000));
   console.log('Hello, World!');
